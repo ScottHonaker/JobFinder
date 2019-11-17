88a4751611556842415f0e10439a2564fdb9fb6f
Jorge A Garcia <betuso12@gmail.com>
2019-11-17 11:20:10 -0700

    login screen visual,logo and icon complete

--- app/src/main/java/com/example/jobfinder/MainActivity.java
+++ app/src/main/java/com/example/jobfinder/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.jobfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Testing");
+        TextView login = findViewById(R.id.loginText);
+        login.setOnClickListener(v -> {
+            startActivity(new Intent(this, ProfileActivity.class));
+        });
     }
 }
