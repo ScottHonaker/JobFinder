40f5515a6915f658258b8f532ec8fa4b01ce5a21
sahonaker <scotthonaker217@gmail.com>
2019-12-03 14:44:50 -0700

    all edits in dashboard activity work in that activity

--- app/src/main/java/com/example/jobfinder/DashboardActivity.java
+++ app/src/main/java/com/example/jobfinder/DashboardActivity.java
@@ -34,6 +34,18 @@ public class DashboardActivity extends AppCompatActivity {
     TextView editDesc;
     TextView userDesc;
 
+    TextView editEmail;
+    TextView userEmail;
+
+    TextView editNumber;
+    TextView userNumber;
+
+    TextView editMessage;
+    TextView userMessage;
+
+    TextView editSocial;
+    TextView userSocial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +73,119 @@ public class DashboardActivity extends AppCompatActivity {
         editDesc.setOnClickListener(edit ->{
             descriptionEdit();
         });
+
+        editEmail = findViewById(R.id.user_edit2);
+        userEmail = findViewById(R.id.user_email);
+        editEmail.setOnClickListener(edit ->{
+            emailEdit();
+        });
+
+        editNumber = findViewById(R.id.user_edit3);
+        userNumber = findViewById(R.id.user_cell);
+        editNumber.setOnClickListener(edit -> {
+            numberEdit();
+        });
+
+        editMessage = findViewById(R.id.user_edit4);
+        userMessage = findViewById(R.id.user_message);
+        editMessage.setOnClickListener(edit -> {
+            messageEdit();
+        });
+
+        editSocial = findViewById(R.id.user_edit5);
+        userSocial = findViewById(R.id.user_add);
+        editSocial.setOnClickListener(edit -> {
+            socialEdit();
+        });
+
+        signOut.setOnClickListener(view ->{
+            firebaseAuth.signOut();
+            checkUserStatus();
+        });
+    }
+
+    private void socialEdit() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardActivity.this);
+        alertDialog.setTitle("Social Media");
+        alertDialog.setMessage("Enter a new social media link");
+
+        final EditText input = new EditText(DashboardActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton("YES",
+                (dialog, which) -> {
+                    userSocial.setText(input.getText().toString());
+                });
+
+        alertDialog.setNegativeButton("NO",
+                (dialog, which) -> dialog.cancel());
+        alertDialog.show();
+    }
+
+    private void messageEdit() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardActivity.this);
+        alertDialog.setTitle("Message");
+        alertDialog.setMessage("Enter a new messenger link");
+
+        final EditText input = new EditText(DashboardActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton("YES",
+                (dialog, which) -> {
+                    userMessage.setText(input.getText().toString());
+                });
+
+        alertDialog.setNegativeButton("NO",
+                (dialog, which) -> dialog.cancel());
+        alertDialog.show();
+    }
+
+    private void numberEdit() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardActivity.this);
+        alertDialog.setTitle("Phone Number");
+        alertDialog.setMessage("Enter a new phone number");
+
+        final EditText input = new EditText(DashboardActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton("YES",
+                (dialog, which) -> {
+                    userNumber.setText(input.getText().toString());
+                });
+
+        alertDialog.setNegativeButton("NO",
+                (dialog, which) -> dialog.cancel());
+        alertDialog.show();
+    }
+
+    private void emailEdit() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardActivity.this);
+        alertDialog.setTitle("Email");
+        alertDialog.setMessage("Enter a new email address");
+
+        final EditText input = new EditText(DashboardActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton("YES",
+                (dialog, which) -> {
+                    userEmail.setText(input.getText().toString());
+                });
+
+        alertDialog.setNegativeButton("NO",
+                (dialog, which) -> dialog.cancel());
+        alertDialog.show();
     }
 
     private void checkUserStatus(){
@@ -76,30 +201,24 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     private void descriptionEdit(){
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardActivity.this);
-            alertDialog.setTitle("Description");
-            alertDialog.setMessage("Enter a new description");
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DashboardActivity.this);
+        alertDialog.setTitle("Description");
+        alertDialog.setMessage("Enter a new description");
 
-            final EditText input = new EditText(DashboardActivity.this);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT);
-            input.setLayoutParams(lp);
-            alertDialog.setView(input);
-            alertDialog.setPositiveButton("YES",
-                    (dialog, which) -> {
-                        userDesc.setText(input.getText().toString());
-                    });
+        final EditText input = new EditText(DashboardActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton("YES",
+                (dialog, which) -> {
+                    userDesc.setText(input.getText().toString());
+                });
 
-            alertDialog.setNegativeButton("NO",
-                    (dialog, which) -> dialog.cancel());
-            alertDialog.show();
-
-
-        signOut.setOnClickListener(view ->{
-            firebaseAuth.signOut();
-            checkUserStatus();
-        });
+        alertDialog.setNegativeButton("NO",
+                (dialog, which) -> dialog.cancel());
+        alertDialog.show();
     }
 
     @Override
