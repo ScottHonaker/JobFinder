4076d3847c7cca51b6c0b7c2dda5b8d5cd92c930
sahonaker <scotthonaker217@gmail.com>
2019-12-02 18:44:39 -0700

    Added bottom nav bar to UserActivity

--- app/src/main/java/com/example/jobfinder/UserActivity.java
+++ app/src/main/java/com/example/jobfinder/UserActivity.java
@@ -2,13 +2,32 @@ package com.example.jobfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class UserActivity extends AppCompatActivity {
+
+    BottomNavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        navView = findViewById(R.id.dashboardNav);
+
+        navView.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) menuItem -> {
+            switch(menuItem.getItemId()){
+                case R.id.nav_home:
+                    startActivity(new Intent(UserActivity.this, DashboardActivity.class));
+                    break;
+                case R.id.nav_users:
+                    startActivity(new Intent(UserActivity.this, BusinessListActivity.class));
+                    break;
+            }
+            return false;
+        });
     }
 }
