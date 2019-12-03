3923d6e67887f36f6771431f8f1ce70e8784c77d
Jorge A Garcia <betuso12@gmail.com>
2019-12-03 16:13:19 -0700

    database linked

--- app/src/main/java/com/example/jobfinder/DashboardActivity.java
+++ app/src/main/java/com/example/jobfinder/DashboardActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,12 +21,26 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.internal.NavigationMenuView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class DashboardActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
-    TextView userN;
+    ImageView avatarImage;
+    TextView nameT,emailT,phoneT;
+
+
+    TextView userMailB;
 
     Button signOut;
 
@@ -35,10 +50,8 @@ public class DashboardActivity extends AppCompatActivity {
     TextView userDesc;
 
     TextView editEmail;
-    TextView userEmail;
-
+    
     TextView editNumber;
-    TextView userNumber;
 
     TextView editMessage;
     TextView userMessage;
@@ -52,8 +65,57 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        userN = findViewById(R.id.profile_email);
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        avatarImage = findViewById(R.id.user_pic);
+        nameT = findViewById(R.id.user_name);
+        emailT = findViewById(R.id.user_email);
+        phoneT = findViewById(R.id.user_cell);
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    String name = "" + ds.child("name").getValue();
+                    String email = "" + ds.child("email").getValue();
+                    String phone = "" + ds.child("phone").getValue();
+                    String image = "" + ds.child("image").getValue();
+
+                    nameT.setText(name);
+                    emailT.setText(email);
+                    phoneT.setText(phone);
+                    try{
+                        Picasso.get().load(image).into(avatarImage);
+
+                    }
+                    catch (Exception e){
+
+                        Picasso.get().load(R.drawable.ic_account_box_black_24dp).into(avatarImage);
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+        userMailB = findViewById(R.id.profile_email);
+
+
+
         signOut = findViewById(R.id.signout);
+
         navView = findViewById(R.id.dashboardNav);
         navView.setSelectedItemId(R.id.nav_home);
         navView.setOnNavigationItemSelectedListener(menuItem -> {
@@ -69,6 +131,7 @@ public class DashboardActivity extends AppCompatActivity {
         });
 
         editDesc = findViewById(R.id.user_edit1);
+
         userDesc = findViewById(R.id.user_description);
         editDesc.setOnClickListener(edit ->{
             descriptionEdit();
@@ -79,13 +142,13 @@ public class DashboardActivity extends AppCompatActivity {
         });
 
         editEmail = findViewById(R.id.user_edit2);
-        userEmail = findViewById(R.id.user_email);
+        emailT = findViewById(R.id.user_email);
         editEmail.setOnClickListener(edit ->{
             emailEdit();
         });
 
         editNumber = findViewById(R.id.user_edit3);
-        userNumber = findViewById(R.id.user_cell);
+        phoneT = findViewById(R.id.user_cell);
         editNumber.setOnClickListener(edit -> {
             numberEdit();
         });
@@ -188,7 +251,7 @@ public class DashboardActivity extends AppCompatActivity {
                     if(input.getText().toString().matches("")){
                         dialog.cancel();
                     } else {
-                        userNumber.setText(input.getText().toString());
+                        phoneT.setText(input.getText().toString());
                     }
         });
         alertDialog.setNegativeButton("NO",
@@ -212,7 +275,7 @@ public class DashboardActivity extends AppCompatActivity {
                     if(input.getText().toString().matches("")){
                         dialog.cancel();
                     } else {
-                        userEmail.setText(input.getText().toString());
+                        emailT.setText(input.getText().toString());
                     }
         });
         alertDialog.setNegativeButton("NO",
@@ -223,7 +286,7 @@ public class DashboardActivity extends AppCompatActivity {
     private void checkUserStatus(){
         FirebaseUser user = firebaseAuth.getCurrentUser();
         if(user != null){
-            userN.setText(user.getEmail());
+            userMailB.setText(user.getEmail());
         }
         else{
             startActivity(new Intent(DashboardActivity.this, MainActivity.class));
--- app/src/main/java/com/example/jobfinder/MainActivity.java
+++ app/src/main/java/com/example/jobfinder/MainActivity.java
@@ -54,13 +54,9 @@ public class MainActivity extends AppCompatActivity {
         register.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, RegisterActivity.class));});
 
-        String[] appNames = new String[] {"Scott", "Jorge"};
-        String[] busiNames = new String[] {"Cheon", "ADP"};
-        String[] appPass = new String[] {"hello", "123A"};
-        String[] busiPass = new String[] {"cs4330", "Pass"};
 
         login.setOnClickListener(v -> {
-            String email = userName.getText().toString();
+            String email = userName.getText().toString().trim();
             String passw = password.getText().toString().trim();
             if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
                 userName.setError("Invalid email");
@@ -78,38 +74,47 @@ public class MainActivity extends AppCompatActivity {
     private void loginUser(String email, String passw) {
         pd.show();
         mAuth.signInWithEmailAndPassword(email, passw)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        pd.dismiss();
-                        // Sign in success, update UI with the signed-in user's information
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        String email1 = user.getEmail();
-                        String uid = user.getUid();
-                        //store user info into data base using hashmap
-                        HashMap<Object, String> hashMap = new HashMap<>();
-                        hashMap.put("email", email1);
-                        hashMap.put("uid", uid);
-                        //////////////////////
-                        hashMap.put("name", "");
-                        hashMap.put("phone", "");
-                        hashMap.put("image", "");
-                        /////database
-                        FirebaseDatabase dataBase = FirebaseDatabase.getInstance();
-                        DatabaseReference reference = dataBase.getReference("Users");
-                        reference.child(uid).setValue(hashMap);
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            pd.dismiss();
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if(task.getResult().getAdditionalUserInfo().isNewUser()){
+                                String email = user.getEmail();
+                                String uid = user.getUid();
+                                //store user info into data base using hashmap
+                                HashMap<Object, String> hashMap = new HashMap<>();
+                                hashMap.put("email", email);
+                                hashMap.put("uid", uid);
+                                //////////////////////
+                                hashMap.put("name", "");
+                                hashMap.put("phone", "");
+                                hashMap.put("image", "");
+                                /////database
+                                FirebaseDatabase dataBase = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = dataBase.getReference("Users");
+                                reference.child(uid).setValue(hashMap);
+                            }
 
-                        //user is logged in
-                        startActivity(new Intent(MainActivity.this,DashboardActivity.class) );
-                        finish();
-                    } else {
-                        pd.dismiss();
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(MainActivity.this, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show();
+
+                            //user is logged in
+                            startActivity(new Intent(MainActivity.this,DashboardActivity.class) );
+                            finish();
+                        } else {
+                            pd.dismiss();
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }).addOnFailureListener(e -> {
-                    pd.dismiss();
-                    Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                });
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
