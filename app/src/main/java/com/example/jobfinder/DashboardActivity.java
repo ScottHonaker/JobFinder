dc4d61866572d47eb6d0b7a269a14dd9513009c1
Jorge A Garcia <betuso12@gmail.com>
2019-12-04 01:32:26 -0700

    last update

--- app/src/main/java/com/example/jobfinder/DashboardActivity.java
+++ app/src/main/java/com/example/jobfinder/DashboardActivity.java
@@ -41,7 +41,6 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
-import java.security.Key;
 import java.util.HashMap;
 
 import static com.google.firebase.storage.FirebaseStorage.getInstance;
--- app/src/main/java/com/example/jobfinder/ProfileActivity.java
+++ app/src/main/java/com/example/jobfinder/ProfileActivity.java
@@ -1,35 +1,71 @@
 package com.example.jobfinder;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.OnBackPressedDispatcherOwner;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import static com.google.firebase.storage.FirebaseStorage.getInstance;
 
 public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    //storage
+    StorageReference storageReference;
 
-    TextView userN;
+    //Profile picture
+    ImageView avatarImage;
+
+    //user name, email, phone
+    TextView nameT,emailT,phoneT;
+
+    //Account: email display
+    TextView userMailB;
 
     Button signOut;
 
-    TextView editDesc;
-    TextView userDesc;
+    BottomNavigationView navView;
+
 
 
     @Override
@@ -38,50 +74,91 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        userN = findViewById(R.id.profile_email);
-        editDesc = findViewById(R.id.user_edit1);
-        userDesc = findViewById(R.id.user_description);
-        editDesc.setOnClickListener(edit ->{
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(ProfileActivity.this);
-            alertDialog.setTitle("PASSWORD");
-            alertDialog.setMessage("Enter Password");
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+        storageReference = getInstance().getReference();
 
-            final EditText input = new EditText(ProfileActivity.this);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT);
-            input.setLayoutParams(lp);
-            alertDialog.setView(input);
-            alertDialog.setPositiveButton("YES",
-                    (dialog, which) -> {
-                        userDesc.setText(input.getText().toString());
-                    });
-            alertDialog.setNegativeButton("NO",
-                    (dialog, which) -> dialog.cancel());
-            alertDialog.show();
+        avatarImage = findViewById(R.id.user_pic);
+        nameT = findViewById(R.id.user_name);
+        emailT = findViewById(R.id.user_email);
+
+
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    String name = "" + ds.child("name").getValue();
+                    String email = "" + ds.child("email").getValue();
+                    String phone = "" + ds.child("phone").getValue();
+                    String image = "" + ds.child("image").getValue();
+
+                    if(!ds.child("name").getValue().toString().matches("")) {
+                        nameT.setText(name);
+                    }
+                    if(!ds.child("email").getValue().toString().matches("")) {
+                        emailT.setText(email);
+                    }
+                    if(!ds.child("phone").getValue().toString().matches("")) {
+                        phoneT.setText(phone);
+                    }
+                    if(!ds.child("image").getValue().toString().matches("")) {
+
+                    }
+                    try{
+                        Picasso.get().load(image).into(avatarImage);
+                    }
+                    catch (Exception e){
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
         });
 
+        userMailB = findViewById(R.id.profile_email);
 
         signOut = findViewById(R.id.signout);
+
+        navView = findViewById(R.id.dashboardNav);
+        navView.setSelectedItemId(R.id.nav_profile);
+        navView.setOnNavigationItemSelectedListener(menuItem -> {
+            switch(menuItem.getItemId()){
+                case R.id.nav_home:
+                    startActivity(new Intent(ProfileActivity.this, DashboardActivity.class));
+                    break;
+                case R.id.nav_users:
+                    startActivity(new Intent(ProfileActivity.this, BusinessListActivity.class));
+                    break;
+            }
+            return false;
+        });
 
         signOut.setOnClickListener(view ->{
             firebaseAuth.signOut();
             checkUserStatus();
-
         });
     }
+
 
     private void checkUserStatus(){
         FirebaseUser user = firebaseAuth.getCurrentUser();
         if(user != null){
-            userN.setText(user.getEmail());
+            userMailB.setText(user.getEmail());
         }
         else{
             startActivity(new Intent(ProfileActivity.this, MainActivity.class));
             finish();
         }
-
     }
+
     @Override
     protected void onStart(){
         checkUserStatus();
